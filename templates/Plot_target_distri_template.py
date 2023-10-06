2517ec881604a854d30e462d389f97aa935a46f5
Xiang Zhang <zhangxiang_planet@outlook.com>
2023-10-06 15:39:41 +0200

    Image adjustment

--- templates/Plot_target_distri_template.py
+++ templates/Plot_target_distri_template.py
@@ -31,12 +31,12 @@ def plot_target_distribution(postprocess_dir, exo_dir):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection=wcs)
 
-    img = ax.imshow(data[0,0], cmap='Blues', vmin=-20, vmax=60)
+    img = ax.imshow(data[0,0], cmap='Blues', vmin=-15, vmax=40)
 
-    ax.scatter(x_target, y_target, s=10, marker='o', color='tab:red', label='Target')
-    ax.scatter(x_exo, y_exo, s=10, marker='+', color='tab:orange', label='Exoplanet')
-    ax.scatter(x_ucd, y_ucd, s=10, marker='*', color='yellow', label='UCD')
-    ax.scatter(x_field, y_field, s=10, marker='.', color='gray', label='Field')
+    ax.scatter(x_target, y_target, s=5, marker='o', color='tab:red', label='Target')
+    ax.scatter(x_exo, y_exo, s=5, marker='+', color='tab:orange', label='Exoplanet')
+    ax.scatter(x_ucd, y_ucd, s=5, marker='*', color='yellow', label='UCD')
+    ax.scatter(x_field, y_field, s=5, marker='.', color='gray', label='Field')
 
     ax.set_xlabel('RA')
     ax.set_ylabel('Dec')
