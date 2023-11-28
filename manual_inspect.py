83b4343f92ccff3d5a7fbdf1d212372f5e0b7176
Xiang Zhang <zhangxiang_planet@outlook.com>
2023-11-28 18:11:26 +0100

    Clean up MSB generated by re-image

--- manual_inspect.py
+++ manual_inspect.py
@@ -311,5 +311,8 @@ for img in img_list:
         cmd_remo_SB = f"rm -rf {postprocess_dir}/{cal_dir}/SB*.MS"
         subprocess.run(cmd_remo_SB, shell=True, check=True)
 
+        cmd_remo_MSB = f"rm -rf {postprocess_dir}/{exo_dir}/MSB_candidate_*"
+        subprocess.run(cmd_remo_MSB, shell=True, check=True)
+
     else:
         continue
