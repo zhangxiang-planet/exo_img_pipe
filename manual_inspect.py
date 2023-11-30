88d608369547dc81a7779653ac766121d13b06e4
Xiang Zhang <zhangxiang_planet@outlook.com>
2023-11-30 13:49:10 +0100

    Reimage 6sigma area

--- manual_inspect.py
+++ manual_inspect.py
@@ -119,7 +119,8 @@ for img in img_list:
 
         # we need the shape of the dynamic spectrum
         num_chan, num_ts = dyna_data.shape
-        four_sigma_mask = np.abs(dyna_data) > 4
+        # Try only image the 6 sigma area
+        four_sigma_mask = np.abs(dyna_data) > 6 
         six_sigma_mask = np.abs(dyna_data) > 6
 
         six_sigma_coords = np.argwhere(six_sigma_mask)
