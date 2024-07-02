67fef3137be5622b8508dd45c14f1438dbf46dd8
Xiang Zhang <zhangxiang_planet@outlook.com>
2024-07-02 09:28:20 +0200

    SL processing

--- manual_drive_SL.py
+++ manual_drive_SL.py
@@ -33,11 +33,11 @@ CALIBRATORS = ['CYG_A', 'CAS_A', 'TAU_A', 'VIR_A']
 # How many SB per processing chunk
 # chunk_num = 12
 
-cal = 'VIR_A'
+cal = 'CYG_A'
 cali_check = False
-cal_dir = '20240621_165900_20240621_171300_VIR_A_TRACKING/L1'
-exo_dir = '20240621_171300_20240621_190000_LEDA_2298219_TRACKING/L1'
-target_name = 'LEDA_2298219'
+cal_dir = '20240625_030000_20240625_031400_CYG_A_TRACKING/L1'
+exo_dir = '20240625_031400_20240625_080000_TYC_2770-733-1_TRACKING/L1'
+target_name = 'TYC_2770-733-1'
 
 # How many channels per SB
 chan_per_SB_origin = 2
