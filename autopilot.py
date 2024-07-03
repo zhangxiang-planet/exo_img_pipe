3885764b60b8d0fb80a72bf873bc0eade41de615
Xiang Zhang <zhangxiang_planet@outlook.com>
2024-07-03 12:18:52 +0200

    XO-3 cycle 2

--- autopilot.py
+++ autopilot.py
@@ -20,7 +20,7 @@ matplotlib.use('Agg')
 ###### Initial settings ######
 
 # Set file locations
-watch_dir = "/databf/nenufar-nri/LT02/202?/??/*WASP-33*"
+watch_dir = "/databf/nenufar-nri/LT02/202?/??/20231208*XO-3*"
 
 preprocess_dir = "/databf/nenufar-nri/LT02/"
 postprocess_dir = "/data/xzhang/exo_img/"
