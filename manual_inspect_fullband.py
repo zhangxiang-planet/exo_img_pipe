09d7d1ceba8f0af5f1e2d66f0e14f977b070d728
Xiang Zhang <zhangxiang_planet@outlook.com>
2025-09-15 13:56:41 +0200

    Bug fix: multiple targets in one observation

--- manual_inspect_fullband.py
+++ manual_inspect_fullband.py
@@ -16,7 +16,7 @@ CALIBRATORS = ['CYG_A', 'CAS_A', 'TAU_A', 'VIR_A']
 
 # the lowest SB we use
 SB_min = 106 # 92
-SB_max = 341
+SB_max = 401
 SB_ave_kms = 2
 
 singularity_command = f"singularity exec -B/data/$USER {singularity_file}"
@@ -149,118 +149,132 @@ for img in img_list:
 
         np.savetxt(f'{watch_dir}{exo_dir}_png_{suffix}/{img_name}.bound_box.txt', unique_bounding_boxes, fmt='%d')
 
-        cmd = f"rsync -av --progress {pre_target_dir}/L1/ {post_target_dir}"
-        subprocess.run(cmd, shell=True, check=True)
+        # Make sure the data hasn't been processed
+        if os.path.exists(f'{postprocess_dir}/{exo_dir}/GSB.MS'):
+            print(f"Data for {exo_dir} has already been processed. Skipping...")
+        else:
 
-        exo_SB_0 = glob.glob(postprocess_dir + exo_dir + '/SB*.MS')
-        # modify this part so the SBs are larger than SB_min and smaller than SB_max
-        exo_SB = [f for f in exo_SB_0 if int(f.split('/SB')[1].split('.MS')[0]) > SB_min and int(f.split('/SB')[1].split('.MS')[0]) < SB_max]
-        # exo_SB = [f for f in exo_SB_0 if int(f.split('/SB')[1].split('.MS')[0]) > SB_min]
-        exo_SB.sort()
+            cmd = f"rsync -av --progress {pre_target_dir}/L1/ {post_target_dir}"
+            subprocess.run(cmd, shell=True, check=True)
 
-        # Now we need to make sure that the number of SB is a multiple of SB_ave_kms. We can remove the first few SBs if necessary
-        num_SB = len(exo_SB)
-        num_remove = num_SB % SB_ave_kms
-        exo_SB = exo_SB[num_remove:]
+            exo_SB_0 = glob.glob(postprocess_dir + exo_dir + '/SB*.MS')
+            # modify this part so the SBs are larger than SB_min and smaller than SB_max
+            exo_SB = [f for f in exo_SB_0 if int(f.split('/SB')[1].split('.MS')[0]) > SB_min and int(f.split('/SB')[1].split('.MS')[0]) < SB_max]
+            # exo_SB = [f for f in exo_SB_0 if int(f.split('/SB')[1].split('.MS')[0]) > SB_min]
+            exo_SB.sort()
 
-        # Group files by their tens place
-        groups = {}
-        for f in exo_SB:
-            sb_number = int(f.split('/SB')[1].split('.MS')[0])
-            tens_place = sb_number // 10
-            if tens_place not in groups:
-                groups[tens_place] = []
-            groups[tens_place].append(f)
+            # Now we need to make sure that the number of SB is a multiple of SB_ave_kms. We can remove the first few SBs if necessary
+            num_SB = len(exo_SB)
+            num_remove = num_SB % SB_ave_kms
+            exo_SB = exo_SB[num_remove:]
 
-        for tens_place, chunk in groups.items():
-            # Extract the ith chunk of chunk_num file names
-            # chunk = exo_SB[i * chunk_num: (i + 1) * chunk_num]
+            # Group files by their tens place
+            groups = {}
+            for f in exo_SB:
+                sb_number = int(f.split('/SB')[1].split('.MS')[0])
+                tens_place = sb_number // 10
+                if tens_place not in groups:
+                    groups[tens_place] = []
+                groups[tens_place].append(f)
 
-            # Create the msin string by joining the chunk with commas
-            SB_str = ",".join(chunk)
+            for tens_place, chunk in groups.items():
+                # Extract the ith chunk of chunk_num file names
+                # chunk = exo_SB[i * chunk_num: (i + 1) * chunk_num]
 
-            # Construct the output file name using the loop index (i+1)
-            MSB_filename = f"{postprocess_dir}/{exo_dir}/MSB{str(tens_place).zfill(2)}.MS"
+                # Create the msin string by joining the chunk with commas
+                SB_str = ",".join(chunk)
 
-            # Construct the command string with the msin argument and the msout argument
-            cmd_flagchan = f"DP3 {pipe_dir}/templates/DPPP-flagchan.parset msin=[{SB_str}] msout={MSB_filename} avg.freqstep={ave_chan} avg.timestep={ave_time}"
-            subprocess.run(cmd_flagchan, shell=True, check=True)
+                # Construct the output file name using the loop index (i+1)
+                MSB_filename = f"{postprocess_dir}/{exo_dir}/MSB{str(tens_place).zfill(2)}.MS"
 
-        # Stack the GSB.MS
-        msb_files = glob.glob(f"{postprocess_dir}/{exo_dir}/MSB*.MS")
-        msb_files.sort()
-        msb_files_str = ",".join(msb_files)
-        cmd_stack = f"DP3 {pipe_dir}/templates/DPPP-stack.parset msin=[{msb_files_str}] msout={postprocess_dir}/{exo_dir}/GSB.MS"
-        subprocess.run(cmd_stack, shell=True, check=True)
+                # Construct the command string with the msin argument and the msout argument
+                cmd_flagchan = f"DP3 {pipe_dir}/templates/DPPP-flagchan.parset msin=[{SB_str}] msout={MSB_filename} avg.freqstep={ave_chan} avg.timestep={ave_time}"
+                subprocess.run(cmd_flagchan, shell=True, check=True)
 
-        cmd_removeMA = f"DP3 {postprocess_dir}/{exo_dir}/DPPP-removeant.parset msin={postprocess_dir}/{exo_dir}/GSB.MS msout={postprocess_dir}/{exo_dir}/GSB.MSB"
-        subprocess.run(cmd_removeMA, shell=True, check=True)
-        # remove original MSB
-        cmd_remo_GSB = f"rm -rf {postprocess_dir}/{exo_dir}/GSB.MS"
-        subprocess.run(cmd_remo_GSB, shell=True, check=True)
-        # rename the new MSB
-        cmd_rename_GSB = f"mv {postprocess_dir}/{exo_dir}/GSB.MSB {postprocess_dir}/{exo_dir}/GSB.MS"
-        subprocess.run(cmd_rename_GSB, shell=True, check=True)
+            # Stack the GSB.MS
+            msb_files = glob.glob(f"{postprocess_dir}/{exo_dir}/MSB*.MS")
+            msb_files.sort()
+            msb_files_str = ",".join(msb_files)
+            cmd_stack = f"DP3 {pipe_dir}/templates/DPPP-stack.parset msin=[{msb_files_str}] msout={postprocess_dir}/{exo_dir}/GSB.MS"
+            subprocess.run(cmd_stack, shell=True, check=True)
 
-        cmd_aoflagger = f"DP3 {pipe_dir}/templates/DPPP-aoflagger.parset msin={postprocess_dir}/{exo_dir}/GSB.MS flag.strategy={pipe_dir}/templates/Nenufar64C1S.lua"
-        subprocess.run(cmd_aoflagger, shell=True, check=True)
+            cmd_removeMA = f"DP3 {postprocess_dir}/{exo_dir}/DPPP-removeant.parset msin={postprocess_dir}/{exo_dir}/GSB.MS msout={postprocess_dir}/{exo_dir}/GSB.MSB"
+            subprocess.run(cmd_removeMA, shell=True, check=True)
+            # remove original MSB
+            cmd_remo_GSB = f"rm -rf {postprocess_dir}/{exo_dir}/GSB.MS"
+            subprocess.run(cmd_remo_GSB, shell=True, check=True)
+            # rename the new MSB
+            cmd_rename_GSB = f"mv {postprocess_dir}/{exo_dir}/GSB.MSB {postprocess_dir}/{exo_dir}/GSB.MS"
+            subprocess.run(cmd_rename_GSB, shell=True, check=True)
 
-        # Copy calibration solution
-        cmd_copy_solution = f'cp {postprocess_dir}/{cal_dir}/GSB.MS/instrument_ddecal.h5 {postprocess_dir}/{exo_dir}/GSB.MS/instrument_dical.h5'
-        subprocess.run(cmd_copy_solution, shell=True, check=True)
+            cmd_aoflagger = f"DP3 {pipe_dir}/templates/DPPP-aoflagger.parset msin={postprocess_dir}/{exo_dir}/GSB.MS flag.strategy={pipe_dir}/templates/Nenufar64C1S.lua"
+            subprocess.run(cmd_aoflagger, shell=True, check=True)
 
-        # apply solution
-        cmd_apply_solution = f'calpipe {pipe_dir}/templates/cali_tran.toml {postprocess_dir}/{exo_dir}/GSB.MS'
-        subprocess.run(cmd_apply_solution, shell=True, check=True)
+            # Copy calibration solution
+            cmd_copy_solution = f'cp {postprocess_dir}/{cal_dir}/GSB.MS/instrument_ddecal.h5 {postprocess_dir}/{exo_dir}/GSB.MS/instrument_dical.h5'
+            subprocess.run(cmd_copy_solution, shell=True, check=True)
 
-        # second round of aoflagger
-        cmd_aoflagger = f"DP3 {pipe_dir}/templates/DPPP-aoflagger.parset msin={postprocess_dir}/{exo_dir}/GSB.MS msin.datacolumn=DI_DATA flag.strategy={pipe_dir}/templates/Nenufar64C1S.lua"
-        subprocess.run(cmd_aoflagger, shell=True, check=True)
+            # apply solution
+            cmd_apply_solution = f'calpipe {pipe_dir}/templates/cali_tran.toml {postprocess_dir}/{exo_dir}/GSB.MS'
+            subprocess.run(cmd_apply_solution, shell=True, check=True)
 
-        # Now we average the GSB.MS into a smaller size
-        cmd_avg = f"DP3 {pipe_dir}/templates/DPPP-average.parset msin={postprocess_dir}/{exo_dir}/GSB.MS msout={postprocess_dir}/{exo_dir}/GSB_avg.MS msin.datacolumn=DI_DATA avg.freqstep={chan_per_SB}"
-        subprocess.run(cmd_avg, shell=True, check=True)
+            # second round of aoflagger
+            cmd_aoflagger = f"DP3 {pipe_dir}/templates/DPPP-aoflagger.parset msin={postprocess_dir}/{exo_dir}/GSB.MS msin.datacolumn=DI_DATA flag.strategy={pipe_dir}/templates/Nenufar64C1S.lua"
+            subprocess.run(cmd_aoflagger, shell=True, check=True)
 
-        # replace the GSB.MS with the averaged one
-        cmd_repl = f"rm -rf {postprocess_dir}/{exo_dir}/GSB.MS"
-        subprocess.run(cmd_repl, shell=True, check=True)
+            # Now we average the GSB.MS into a smaller size
+            cmd_avg = f"DP3 {pipe_dir}/templates/DPPP-average.parset msin={postprocess_dir}/{exo_dir}/GSB.MS msout={postprocess_dir}/{exo_dir}/GSB_avg.MS msin.datacolumn=DI_DATA avg.freqstep={chan_per_SB}"
+            subprocess.run(cmd_avg, shell=True, check=True)
 
-        cmd_repl = f"mv {postprocess_dir}/{exo_dir}/GSB_avg.MS {postprocess_dir}/{exo_dir}/GSB.MS"
-        subprocess.run(cmd_repl, shell=True, check=True)
+            # replace the GSB.MS with the averaged one
+            cmd_repl = f"rm -rf {postprocess_dir}/{exo_dir}/GSB.MS"
+            subprocess.run(cmd_repl, shell=True, check=True)
 
-        # we need the number of beams for the following steps
-        num_SB = len(exo_SB)
-        num_beam = int(num_SB / SB_ave_kms)
+            cmd_repl = f"mv {postprocess_dir}/{exo_dir}/GSB_avg.MS {postprocess_dir}/{exo_dir}/GSB.MS"
+            subprocess.run(cmd_repl, shell=True, check=True)
 
-        cmd_ddf = (
-            f'DDF.py {pipe_dir}/templates/template_DI.parset --Data-MS {postprocess_dir}{exo_dir}/GSB.MS --Data-ColName DATA --Output-Name {postprocess_dir}{exo_dir}/Image_DI_Bis '
-            f'--Cache-Reset 1 --Cache-Dir {postprocess_dir}{exo_dir}/. --Deconv-Mode SSD2 --Mask-Auto 1 --Mask-SigTh 7 --Deconv-MaxMajorIter 3 --Deconv-RMSFactor 3 --Deconv-PeakFactor 0.1 --Facets-NFacet 1 --Facets-DiamMax 5 '
-            f'--Weight-OutColName DDF_WEIGHTS --GAClean-ScalesInitHMP [0] --Beam-Model None '
-            f'--Freq-NBand {num_beam} --SSD2-PolyFreqOrder 3 --Freq-NDegridBand 0 --Image-NPix 1200 --Image-Cell 120 --Data-ChunkHours 0.5'
-        )
-        combined_ddf = f"{singularity_command} {cmd_ddf}"
-        subprocess.run(combined_ddf, shell=True, check=True)
+            # we need the number of beams for the following steps
+            num_SB = len(exo_SB)
+            num_beam = int(num_SB / SB_ave_kms)
 
-        cmd_mask = (
-            f'MakeMask.py --RestoredIm {postprocess_dir}{exo_dir}/Image_DI_Bis.app.restored.fits --Box 100,2 --Th 10000 --ds9Mask {region_file}'
-        )
-        combined_mask = f"{singularity_command} {cmd_mask}"
-        subprocess.run(combined_mask, shell=True, check=True)
-        
-        # Remove ATeam from DicoModel
-        cmd_maskdico = (
-            f'MaskDicoModel.py --InDicoModel {postprocess_dir}{exo_dir}/Image_DI_Bis.DicoModel --OutDicoModel {postprocess_dir}{exo_dir}/Image_DI_Bis.filterATeam.DicoModel --MaskName {postprocess_dir}{exo_dir}/Image_DI_Bis.app.restored.fits.mask.fits --InvertMask 1'
-        )
-        combined_maskdico = f"{singularity_command} {cmd_maskdico}"
-        subprocess.run(combined_maskdico, shell=True, check=True)
+            cmd_ddf = (
+                f'DDF.py {pipe_dir}/templates/template_DI.parset --Data-MS {postprocess_dir}{exo_dir}/GSB.MS --Data-ColName DATA --Output-Name {postprocess_dir}{exo_dir}/Image_DI_Bis '
+                f'--Cache-Reset 1 --Cache-Dir {postprocess_dir}{exo_dir}/. --Deconv-Mode SSD2 --Mask-Auto 1 --Mask-SigTh 7 --Deconv-MaxMajorIter 3 --Deconv-RMSFactor 3 --Deconv-PeakFactor 0.1 --Facets-NFacet 1 --Facets-DiamMax 5 '
+                f'--Weight-OutColName DDF_WEIGHTS --GAClean-ScalesInitHMP [0] --Beam-Model None '
+                f'--Freq-NBand {num_beam} --SSD2-PolyFreqOrder 3 --Freq-NDegridBand 0 --Image-NPix 1200 --Image-Cell 120 --Data-ChunkHours 0.5'
+            )
+            combined_ddf = f"{singularity_command} {cmd_ddf}"
+            subprocess.run(combined_ddf, shell=True, check=True)
 
-        cmd_kms = (
-            f'kMS.py --MSName {postprocess_dir}{exo_dir}/GSB.MS --SolverType CohJones --PolMode IFull --BaseImageName {postprocess_dir}{exo_dir}/Image_DI_Bis --dt 1 --InCol DATA --SolsDir={postprocess_dir}{exo_dir}/SOLSDIR --NodesFile Single --DDFCacheDir={postprocess_dir}{exo_dir}/ '
-            f'--NChanPredictPerMS {num_beam} --NChanSols {num_beam} --OutSolsName DD1 --UVMinMax 0.067,1000 --AppendCalSource All --FreePredictGainColName KMS_SUB:data-ATeam '
-            f'--DicoModel {postprocess_dir}{exo_dir}/Image_DI_Bis.filterATeam.DicoModel --WeightInCol DDF_WEIGHTS'
-        )
-        combined_kms = f"{singularity_command} {cmd_kms}"
-        subprocess.run(combined_kms, shell=True, check=True)
+            cmd_mask = (
+                f'MakeMask.py --RestoredIm {postprocess_dir}{exo_dir}/Image_DI_Bis.app.restored.fits --Box 100,2 --Th 10000 --ds9Mask {region_file}'
+            )
+            combined_mask = f"{singularity_command} {cmd_mask}"
+            subprocess.run(combined_mask, shell=True, check=True)
+            
+            # Remove ATeam from DicoModel
+            cmd_maskdico = (
+                f'MaskDicoModel.py --InDicoModel {postprocess_dir}{exo_dir}/Image_DI_Bis.DicoModel --OutDicoModel {postprocess_dir}{exo_dir}/Image_DI_Bis.filterATeam.DicoModel --MaskName {postprocess_dir}{exo_dir}/Image_DI_Bis.app.restored.fits.mask.fits --InvertMask 1'
+            )
+            combined_maskdico = f"{singularity_command} {cmd_maskdico}"
+            subprocess.run(combined_maskdico, shell=True, check=True)
+
+            cmd_kms = (
+                f'kMS.py --MSName {postprocess_dir}{exo_dir}/GSB.MS --SolverType CohJones --PolMode IFull --BaseImageName {postprocess_dir}{exo_dir}/Image_DI_Bis --dt 1 --InCol DATA --SolsDir={postprocess_dir}{exo_dir}/SOLSDIR --NodesFile Single --DDFCacheDir={postprocess_dir}{exo_dir}/ '
+                f'--NChanPredictPerMS {num_beam} --NChanSols {num_beam} --OutSolsName DD1 --UVMinMax 0.067,1000 --AppendCalSource All --FreePredictGainColName KMS_SUB:data-ATeam '
+                f'--DicoModel {postprocess_dir}{exo_dir}/Image_DI_Bis.filterATeam.DicoModel --WeightInCol DDF_WEIGHTS'
+            )
+            combined_kms = f"{singularity_command} {cmd_kms}"
+            subprocess.run(combined_kms, shell=True, check=True)
+
+            cmd_remo_SB = f"rm -rf {postprocess_dir}/{exo_dir}/SB*.MS"
+            subprocess.run(cmd_remo_SB, shell=True, check=True)
+
+            cmd_remo_MSB = f"rm -rf {postprocess_dir}/{exo_dir}/MSB*.MS"
+            subprocess.run(cmd_remo_MSB, shell=True, check=True)
+
+            cmd_remo_soldir = f"rm -rf {postprocess_dir}/{exo_dir}/SOLSDIR"
+            subprocess.run(cmd_remo_soldir, shell=True, check=True)
 
         # we might have multiple detections within one dynamic spectrum
         for i in range(len(unique_bounding_boxes)):
@@ -302,15 +316,6 @@ for img in img_list:
                                 f'-local-rms -join-polarizations -multiscale -no-negative -no-update-model-required -no-dirty '
                                 f'-interval {max_time+1} {max_time+num_time+1} -channel-range {min_freq} {max_freq+1} -name {postprocess_dir}/{exo_dir}/MSB_{min_freq}_{max_freq}_{min_time}_{max_time}_post {postprocess_dir}/{exo_dir}/GSB.MS')
                 subprocess.run(cmd_post_img, shell=True, check=True)
-
-        cmd_remo_SB = f"rm -rf {postprocess_dir}/{exo_dir}/SB*.MS"
-        subprocess.run(cmd_remo_SB, shell=True, check=True)
-
-        cmd_remo_MSB = f"rm -rf {postprocess_dir}/{exo_dir}/MSB*.MS"
-        subprocess.run(cmd_remo_MSB, shell=True, check=True)
-
-        cmd_remo_soldir = f"rm -rf {postprocess_dir}/{exo_dir}/SOLSDIR"
-        subprocess.run(cmd_remo_soldir, shell=True, check=True)
 
     else:
         continue
